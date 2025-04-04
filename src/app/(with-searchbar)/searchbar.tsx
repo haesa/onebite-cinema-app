859b40f5d4aef14b50cb9f125a4a61fb533315b0
haesa <xdhaesa@gmail.com>
2025-04-05 00:58:33 +0900

    feat(Day10): 서치 레이아웃에서 서버 컴포넌트와 클라이언트 컴포넌트 분리

---
+++ src/app/(with-searchbar)/searchbar.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Searchbar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const q = searchParams.get('q');
+  const [search, setSearch] = useState(q || '');
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSearch = () => {
+    if (!search || search === q) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+        placeholder='검색어를 입력하세요'
+      />
+      <button type='button' onClick={onSearch}>
+        검색
+      </button>
+    </div>
+  );
+}
