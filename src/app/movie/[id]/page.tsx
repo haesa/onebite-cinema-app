5eeb5205d809f154f3a6aaf976ed22869c013404
haesa <xdhaesa@gmail.com>
2025-04-17 22:55:27 +0900

    fix(Day19): params type 수정

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -110,12 +110,18 @@ export async function generateMetadata({
   };
 }
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   return (
     <div className={style.container}>
-      <MovieDetail movieId={params.id} />
-      <ReviewEditor movieId={params.id} />
-      <ReviewList movieId={params.id} />
+      <MovieDetail movieId={id} />
+      <ReviewEditor movieId={id} />
+      <ReviewList movieId={id} />
     </div>
   );
 }
